f569d2bfdb795ce55b5edfa2a283df57201843fd
uekann <kantaueno0808@yahoo.co.jp>
2022-07-06 04:29:16 +0900

    "get_places_to_put"の実装と"__str__"の実装

--- ReversiAI/lib/riversi.py
+++ ReversiAI/lib/riversi.py
@@ -9,17 +9,13 @@ class board():
 
     def __init__(self) -> None:
         """コンストラクタ"""
-        self.board = np.zeros((8,8))              #盤面の作成
+        self.board = np.zeros((8,8),dtype=np.int32)              #盤面の作成
         self.board[[3,4],[3,4]] = board.WHITE     #白い駒の配置
         self.board[[4,3],[3,4]] = board.BLACK     #黒い駒の配置
-        self.number_W = 2                         #白い駒の数
-        self.number_B = 2                         #黒い駒の数
-
-        return None
+        self.numbers = {board.WHITE : 2, board.BLACK : 2}  # 駒数の初期化
     
-
     @classmethod
-    def _is_color(cls, color) -> bool:
+    def _is_color(cls, color) -> None:
         """渡された値がWHITEかBLACKのいずれかであるかを判定"""
 
         if not type(color) is int:
@@ -29,12 +25,10 @@ class board():
         if not (color == board.WHITE or color == board.BLACK):
             raise ValueError(f"{color} is not color. color should be {board.WHITE} or {board.BLACK}")
         
-
-        return True
     
 
     @classmethod
-    def _is_place(cls, place) -> bool:
+    def _is_place(cls, place) -> None:
         """placeが盤面上の座標であるか判定"""
 
         if not type(place) is tuple:
@@ -45,8 +39,6 @@ class board():
         
         if not(0 <= place[0] <= 7 and 0 <= place[1] <= 7):
             raise IndexError("Place out of range.")
-        
-        return True
     
 
     @classmethod
@@ -75,32 +67,33 @@ class board():
         board._is_color(color)
         board._is_place(place)
 
-        if self.board[place] != 0:
+        if self.board[place] != 0:  # 既に置かれていたら[]を返す
             return []
 
-        change_plases = []
+        change_plases = []    # ひっくり返る座標のリスト
+
         for direction in [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]:
-            change_plases_ = []
+            change_plases_ = []   # directionの方向でひっくり返る駒
             search_place = place
             while True:
-                search_place = (search_place[0] + direction[0], search_place[1] + direction[1])
+                search_place = (search_place[0] + direction[0], search_place[1] + direction[1])  # 探索場所の更新
             
-                if not(0 <= search_place[0] <= 7 and 0 <= search_place[1] <= 7) or self.board[search_place] == 0:
+                if not(0 <= search_place[0] <= 7 and 0 <= search_place[1] <= 7) or self.board[search_place] == 0:  # 行き止まりなら[]を返す
                     change_plases_ = []
                     break
 
-                if self.board[search_place] == color:
+                if self.board[search_place] == color:     # colorに出逢ったら終了
                     break
 
-                if self.board[search_place] != color:
+                if self.board[search_place] != color:     # colorの反対の色ならその座標を保存して探索を続ける
                     change_plases_.append(search_place)
             
-            change_plases += change_plases_
-
+            change_plases += change_plases_       # 得られた座標をまとめてchange_plasesに保存
 
         return change_plases
     
-    def put(self, place, color) -> bool:
+
+    def put(self, place, color) -> None:
         """コマを置く
 
         Parameters
@@ -121,22 +114,59 @@ class board():
 
         change_places = self.get_change_plases(place, color)
 
-        if change_places == []:
+        if change_places == []:         # ひっくり返る場所がなければ置けない(例外を出力)
             raise ValueError("You cannot place a piece there.")
-            return False
         
         for change_place in change_places:
-            self.board[change_place] = color
-            
-        self.board[place] = color
-        return True
+            self.board[change_place] = color  # 色の変更
+
+        self.board[place] = color     # 駒を置く
+
+        self.numbers[color] += 1
+        self.numbers[board._turn_color(color)] -= len(change_places)  # 駒数の更新
+    
+    
+    def get_places_to_put(self, color) -> list:
+        """置ける場所の取得
+
+        Parameters
+        ----------
+        color : int
+            置く色
+
+        Returns
+        -------
+        list
+            おける場所のリスト
+        """
+
+        board._is_color(color)
+
+        places_to_put = []
+    
+        for i in range(8):
+            for j in range(8):
+                if not self.get_change_plases((i, j), color) == []:
+                    places_to_put.append((i, j))
+        
+        return places_to_put
+
+    def __str__(self) -> str:
+        s = ""
+        for i in self.board:
+            s += " ".join(map(str, i)) + "\n"
+        
+        s += f"Black | Point:{self.numbers[board.BLACK]}     Place to put:{self.get_places_to_put(board.BLACK)}\n"
+        s += f"White | Point:{self.numbers[board.WHITE]}     Place to put:{self.get_places_to_put(board.WHITE)}"
+
+        return s
+
+
 
 if __name__ == "__main__":
     bd = board()
-    color = board.WHITE
-
+    color = board.BLACK
     while True:
-        print(bd.board)
-        point = tuple(map(int, input().split()))
-        bd.put(point,color)
+        print(bd)
+        bd.put(tuple(map(int, input("put:").split())), color)
         color = board._turn_color(color)
